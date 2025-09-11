295c48c55f54ebdc70aec626856d85ef34664529
Edson Koguishi <edsonkoguishi@gmail.com>
2025-09-10 21:24:48 -0300

    migrations

---
+++ Migrations/20250911002110_Categorias_DeleteBehavior_Cascade.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kendo_londrina.Migrations
+{
+    /// <inheritdoc />
+    public partial class Categorias_DeleteBehavior_Cascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategorias_Categorias_CategoriaId",
+                table: "SubCategorias");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategorias_Categorias_CategoriaId",
+                table: "SubCategorias",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategorias_Categorias_CategoriaId",
+                table: "SubCategorias");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategorias_Categorias_CategoriaId",
+                table: "SubCategorias",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
