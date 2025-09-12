0072f7a5548d454154201c4f747111a7b707ad40
Edson Koguishi <edsonkoguishi@gmail.com>
2025-09-12 16:16:26 -0300

    feat: Repository ContaPagar ContaReceber

---
+++ Infra/Repositories/ContaPagarRepository.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using kendo_londrina.Domain.Entities;
+using kendo_londrina.Domain.Repositories;
+using kendo_londrina.Infra.Data;
+
+namespace kendo_londrina.Infrastructure.Repositories
+{
+    public class ContaPagarRepository : IContaPagarRepository
+    {
+        private readonly KendoLondrinaContext _context;
+
+        public ContaPagarRepository(KendoLondrinaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<ContaPagar?> GetByIdAsync(Guid empresaId, Guid id)
+        {
+            var contasPagar = await _context.ContasPagar
+                .Where(p => p.EmpresaId == empresaId && p.Id == id)
+                .FirstOrDefaultAsync();
+            return contasPagar;
+        }
+
+        public async Task AddAsync(ContaPagar contaPagar)
+        {
+            await _context.ContasPagar.AddAsync(contaPagar);
+        }
+
+        public Task DeleteAsync(ContaPagar contaPagar)
+        {
+            _context.ContasPagar.Remove(contaPagar);
+            return _context.SaveChangesAsync();
+        }
+
+        Task<List<ContaPagar>> IContaPagarRepository.GetAllAsync(Guid empresaId)
+        {
+            return _context.ContasPagar
+                .Where(p => p.EmpresaId == empresaId).ToListAsync();
+        }
+
+        IQueryable<ContaPagar> IContaPagarRepository.Query(Guid empresaId)
+        {
+            return _context.ContasPagar
+                .Where(p => p.EmpresaId == empresaId).AsQueryable();
+        }
+    }
+}
---
+++ Infra/Repositories/ContaReceberRepository.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using kendo_londrina.Domain.Entities;
+using kendo_londrina.Domain.Repositories;
+using kendo_londrina.Infra.Data;
+
+namespace kendo_londrina.Infrastructure.Repositories
+{
+    public class ContaReceberRepository : IContaReceberRepository
+    {
+        private readonly KendoLondrinaContext _context;
+
+        public ContaReceberRepository(KendoLondrinaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<ContaReceber?> GetByIdAsync(Guid empresaId, Guid id)
+        {
+            var contaReceber = await _context.ContasReceber
+                .Where(p => p.EmpresaId == empresaId && p.Id == id)
+                .FirstOrDefaultAsync();
+            return contaReceber;
+        }
+
+        public async Task AddAsync(ContaReceber contaReceber)
+        {
+            await _context.ContasReceber.AddAsync(contaReceber);
+        }
+
+        public Task DeleteAsync(ContaReceber contaReceber)
+        {
+            _context.ContasReceber.Remove(contaReceber);
+            return _context.SaveChangesAsync();
+        }
+
+        Task<List<ContaReceber>> IContaReceberRepository.GetAllAsync(Guid empresaId)
+        {
+            return _context.ContasReceber
+                .Where(p => p.EmpresaId == empresaId).ToListAsync();
+        }
+
+        IQueryable<ContaReceber> IContaReceberRepository.Query(Guid empresaId)
+        {
+            return _context.ContasReceber
+                .Where(p => p.EmpresaId == empresaId).AsQueryable();
+        }
+    }
+}
